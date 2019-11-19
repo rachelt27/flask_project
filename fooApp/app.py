7788e5e21d42b40f58f64004d87222ce97f8ca7f
rachel27 <racheltrimble89@gmail.com>
2019-11-19 21:06:59 +0100

    add display product functionality add edit product

--- fooApp/app.py
+++ fooApp/app.py
@@ -1,21 +1,42 @@
-from flask import Flask, render_template
+from bson.objectid import ObjectId
+from flask import Flask, render_template, abort, redirect, request, url_for
+from flask_pymongo import PyMongo
+
+from fooApp.forms import ProductForm
 
 app = Flask(__name__)
+
+app.config['MONGO_DBNAME'] = 'foodb'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/foodb'
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return redirect(url_for('products_list'))
 
 
 @app.route('/products/')
 def products_list():
-    return 'Listing of all products we have.'
+    """Provide HTML listing of all Products."""
+    # Query: Get all Products objects, sorted by date.
+    products = mongo.db.products.find()[:]
+    return render_template('product/index.html',
+                       products=products)
 
 
 @app.route('/products/<product_id>/')
 def product_detail(product_id):
-    return 'Detail of product     #{}.'.format(product_id)
+    """Provide HTML page with a given product."""
+    # Query: get Product object by ID.
+    product = mongo.db.products.find_one({"_id": ObjectId(product_id)})
+    print(product)
+    if product is None:
+        # Abort with Not Found.
+        abort(404)
+    return render_template('product/detail.html',
+                           product=product)
 
 
 @app.route(
@@ -27,12 +48,26 @@ def product_edit(product_id):
 
 @app.route('/products/create/', methods=['GET', 'POST'])
 def product_create():
-    return 'Form to create a new product.'
+    """Provide HTML form to create a new product."""
+    form = ProductForm(request.form)
+    if request.method == 'POST' and form.validate():
+        mongo.db.products.insert_one(form.data)
+        # Success. Send user back to full product list.
+        return redirect(url_for('products_list'))
+    # Either first load or validation error at this point.
+    return render_template('product/edit.html', form=form)
 
 
 @app.route('/products/<product_id>/delete/', methods=['DELETE'])
 def product_delete(product_id):
-    raise NotImplementedError('DELETE')
+    """Delete record using HTTP DELETE, respond with JSON."""
+    result = mongo.db.products.delete_one({ "_id": ObjectId(product_id) })
+    if result.deleted_count == 0:
+        # Abort with Not Found, but with simple JSON response.
+        response = jsonify({'status': 'Not Found'})
+        response.status = 404
+        return response
+    return jsonify({'status': 'OK'})
 
 
 if __name__ == "__main__":
